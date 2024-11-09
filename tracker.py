80fcf3a2ba041d4951a99ad28de8f67912248f61
trungleminh2909 <trung.leminh@hcmut.edu.vn>
2024-11-09 12:08:42 +0700

    tracker trung-branch

--- tracker.py
+++ tracker.py
@@ -5,8 +5,30 @@ TIMEOUT = 2
 class Tracker:
     host = None
     port = None
-    TrackerSocket = None
-    TrackerDb = None
-    TrackerOn = True
+    socket = None
+    ClientList = []       
+    TrackerOn = False
 
-    def __init__(self, host = None, port = None, TrackerSocket = None, TrackerDb = None)
+    def __init__(self, host = None, port = None, socket = None, ClientList = []):
+        #Turn on tracker
+        TrackerOn = True
+        
+        #Initialize tracker data
+        self.host = host
+        self.port = port
+        self.socket = socket
+        self.ClientList = ClientList
+
+    def addClient (self, clientID = None):
+        if(clientID != None):
+            self.ClientList.append(clientID)
+        return f"Client {clientID} successfully joined the torrent network."
+
+    def deleteClient(self, clientID):
+        if clientID in self.ClientList:
+            self.ClientList.remove(clientID)
+            return f"Client {clientID} left the torrent network."
+        else:
+            return f"Client {clientID} not exist in the torrent network."
+
+
