ebd8e966a854eb69df328a5d011ffa7b98dd3ffc
trungleminh2909 <trung.leminh@hcmut.edu.vn>
2024-11-09 11:50:29 +0700

    tracker trung-branch

--- tracker.py
+++ tracker.py
@@ -1,1 +1,12 @@
-#
+#Define timeout
+TIMEOUT = 2
+
+#Tracker class
+class Tracker:
+    host = None
+    port = None
+    TrackerSocket = None
+    TrackerDb = None
+    TrackerOn = True
+
+    def __init__(self, host = None, port = None, TrackerSocket = None, TrackerDb = None)
